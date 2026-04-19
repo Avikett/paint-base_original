eb5ac3beb768fd606d17b89d833a790f083cfcad
Avikett <44085330+Avikett@users.noreply.github.com>
2026-04-20 02:57:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const canvas = document.querySelector("#board");
+const ctx = canvas.getContext("2d");
+let isDrawing = false;
+
+// Технічні параметри пензля
+ctx.lineWidth = 20;
+ctx.lineCap = "round";
+ctx.strokeStyle = "darkgreen"; // Червоний колір за замовчуванням
+
+// Логіка малювання
+canvas.onmousedown = (e) => {
+  isDrawing = true;
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+};
+canvas.onmouseup = () => (isDrawing = false);
+canvas.onmousemove = (e) => {
+  if (isDrawing) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  }
+};
